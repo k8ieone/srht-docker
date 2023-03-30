8270f2666d23c11e27482866d353812d3a8c5810
Jakob Meier <comcloudway@ccw.icu>
2023-03-30 15:04:18 +0200

    Added experimental sourcehut-hub support

---
+++ services/hub.sr.ht/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# hub.sr.ht-web
+# https://git.sr.ht/~sircmpwn/sr.ht-apkbuilds/tree/master/item/sr.ht/hub.sr.ht/hub.sr.ht.initd
+/usr/bin/gunicorn hubsrht.app:app \
+    -c /etc/sr.ht/hub.sr.ht.gunicorn.conf.py \
+    -b 0.0.0.0:5014 \
+    -D
+
+# hub.sr.ht-api
+# https://git.sr.ht/~sircmpwn/sr.ht-apkbuilds/tree/master/item/sr.ht/hub.sr.ht/hub.sr.ht-api.initd
+/usr/bin/hubsrht-api \
+    -b 0.0.0.0:5114 &
+
+nginx &
+
+tail -f /dev/null
