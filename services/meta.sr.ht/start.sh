85bb63ba57338ae4bd87df7e07e73f715ed8bced
a13xie@Sodium <rostik.medved@gmail.com>
2023-05-21 22:21:11 +0200

    Trying to fix the meta app

--- services/meta.sr.ht/start.sh
+++ services/meta.sr.ht/start.sh
@@ -5,19 +5,21 @@ metasrht-initdb
 
 # meta.sr.ht
 # https://git.sr.ht/~sircmpwn/sr.ht-apkbuilds/tree/master/item/sr.ht/meta.sr.ht/meta.sr.ht.initd
-/usr/bin/gunicorn metasrht.app:app \
+mkdir /run/meta.sr.ht
+chown -R srht:srht /run/meta.sr.ht
+chmod 775 /run/meta.sr.ht
+sudo -u srht prometheus_multiproc_dir=/run/meta.sr.ht /usr/bin/gunicorn metasrht.app:app \
     -b 0.0.0.0:5000 \
-    -c /etc/sr.ht/meta.sr.ht.gunicorn.conf.py \
-    -D
+    -c /etc/sr.ht/meta.sr.ht.gunicorn.conf.py &
 
 # meta.sr.ht-api
 # https://git.sr.ht/~sircmpwn/sr.ht-apkbuilds/tree/master/item/sr.ht/meta.sr.ht/meta.sr.ht-api.initd
-/usr/bin/metasrht-api \
+sudo -u srht /usr/bin/metasrht-api \
     -b 0.0.0.0:5100 &
 
 # meta.sr.ht-webhooks
 # https://git.sr.ht/~sircmpwn/sr.ht-apkbuilds/tree/master/item/sr.ht/meta.sr.ht/meta.sr.ht-webhooks.initd
-/usr/bin/celery \
+sudo -u srht /usr/bin/celery \
     -A metasrht.webhooks worker \
     --loglevel=info &
 
